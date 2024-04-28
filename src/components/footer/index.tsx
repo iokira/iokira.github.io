18f2bba9d20f4fb7c4c276805eef5b837e85b4a4
iokira <iam.iokiller@gmail.com>
2024-04-29 01:56:12 +0900

    add header and footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+    return <footer>&copy;iokira 2024</footer>
+}
+
+export default Footer
---
+++ src/components/header/index.tsx
@@ -0,0 +1,11 @@
+import { Link } from "@chakra-ui/react"
+
+const Header = () => {
+    return (
+        <header>
+            <Link href="/">iokira.github.io</Link>
+        </header>
+    )
+}
+
+export default Header
