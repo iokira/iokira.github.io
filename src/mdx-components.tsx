5aca3c03c5003624aa8dab4278d309e8c071e20a
iokira <iam.iokiller@gmail.com>
2024-04-29 02:07:49 +0900

    add heading style

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -3,7 +3,54 @@ import type { MDXComponents } from "mdx/types"
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
     return {
-        h1: ({ children }) => <Heading>{children}</Heading>,
+        h1: ({ children }) => (
+            <Heading
+                as="h1"
+                size="4xl"
+            >
+                {children}
+            </Heading>
+        ),
+        h2: ({ children }) => (
+            <Heading
+                as="h2"
+                size="3xl"
+            >
+                {children}
+            </Heading>
+        ),
+        h3: ({ children }) => (
+            <Heading
+                as="h3"
+                size="2xl"
+            >
+                {children}
+            </Heading>
+        ),
+        h4: ({ children }) => (
+            <Heading
+                as="h4"
+                size="xl"
+            >
+                {children}
+            </Heading>
+        ),
+        h5: ({ children }) => (
+            <Heading
+                as="h5"
+                size="lg"
+            >
+                {children}
+            </Heading>
+        ),
+        h6: ({ children }) => (
+            <Heading
+                as="h6"
+                size="md"
+            >
+                {children}
+            </Heading>
+        ),
         ...components,
     }
 }
