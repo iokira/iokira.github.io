e73c4f2e38a3a5a74b29bb4aa73f8b874a00f581
iokira <iam.iokiller@gmail.com>
2024-04-28 18:38:24 +0900

    add _app.tsx _document.tsx

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, NextScript, Main } from "next/document"
+
+const Document = () => {
+    return (
+        <Html>
+            <Head />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
+
+export default Document
