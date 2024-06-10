7e554e369fdac95abc34d750085a4b2df6c51faa
iokira <iam.iokiller@gmail.com>
2024-06-10 17:12:29 +0900

    update: mdx component h

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -3,12 +3,12 @@ import Link from "next/link"
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
     return {
-        h1: ({ children }) => <header>{children}</header>,
-        h2: ({ children }) => <header>{children}</header>,
-        h3: ({ children }) => <header>{children}</header>,
-        h4: ({ children }) => <header>{children}</header>,
-        h5: ({ children }) => <header>{children}</header>,
-        h6: ({ children }) => <header>{children}</header>,
+        h1: ({ children }) => <h1>{children}</h1>,
+        h2: ({ children }) => <h2>{children}</h2>,
+        h3: ({ children }) => <h3>{children}</h3>,
+        h4: ({ children }) => <h4>{children}</h4>,
+        h5: ({ children }) => <h5>{children}</h5>,
+        h6: ({ children }) => <h6>{children}</h6>,
         ul: ({ children }) => <ul>{children}</ul>,
         ol: ({ children }) => <ol>{children}</ol>,
         li: ({ children }) => <li>{children}</li>,
