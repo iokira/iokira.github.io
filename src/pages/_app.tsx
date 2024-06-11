6a4b5eea814cfef5852b29ffb8cbfab3893fc297
iokira <iam.iokiller@gmail.com>
2024-06-11 11:09:21 +0900

    update: paths

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
-import "./global.scss"
+import "@globals"
+import styles from "@/styles/_app.module.scss"
 import type { AppProps } from "next/app"
-import Header from "../components/header"
-import Footer from "../components/footer"
+import Header from "@/components/header"
+import Footer from "@/components/footer"
 
 const App = ({ Component, pageProps }: AppProps) => {
     return (
-        <main>
+        <main className={styles.main}>
             <Header />
             <Component {...pageProps} />
             <Footer />
