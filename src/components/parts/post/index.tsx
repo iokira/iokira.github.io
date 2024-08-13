c62b263e2b686426d2d02e4190216fefbe270757
iokira <iam.iokiller@gmail.com>
2024-08-13 21:30:09 +0900

    update: 記事のスタイルを調整

--- src/components/parts/post/index.tsx
+++ src/components/parts/post/index.tsx
@@ -4,9 +4,11 @@ import styles from "./style.module.scss"
 const Post = ({ post }: { post: Post }) => {
     return (
         <article className={styles.post}>
-            <p>{post.data.updateDate}</p>
-            <Link href={"blog/posts/" + post.id}>{post.data.title}</Link>
-            <p>{post.data.description}</p>
+            <p className={styles.date}>{post.data.updateDate}</p>
+            <div className={styles.title}>
+                <Link href={"blog/posts/" + post.id}>{post.data.title}</Link>
+            </div>
+            <p className={styles.description}>{post.data.description}</p>
         </article>
     )
 }
--- src/components/parts/posts/index.tsx
+++ src/components/parts/posts/index.tsx
@@ -1,12 +1,17 @@
 import Post from "@/components/parts/post"
+import styles from "./style.module.scss"
 
 const Posts = ({ posts }: { posts: Post[] }) => {
-    return posts.map((post) => (
-        <Post
-            key={post.id}
-            post={post}
-        />
-    ))
+    return (
+        <div className={styles.posts}>
+            {posts.map((post) => (
+                <Post
+                    key={post.id}
+                    post={post}
+                />
+            ))}
+        </div>
+    )
 }
 
 export default Posts
