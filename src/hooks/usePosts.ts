1422669787c0f64d9e8ecf4ce83bd4dd23f241d4
iokira <iam.iokiller@gmail.com>
2024-08-13 18:23:49 +0900

    add: 記事を取得するhookを作成

---
+++ src/hooks/usePosts.ts
@@ -0,0 +1,35 @@
+import { readFileSync, readdirSync } from "fs"
+import matter from "gray-matter"
+import path from "path"
+
+const postDir = path.join(process.cwd(), "src/posts")
+
+const usePosts = () => {
+    const getPosts = async () => {
+        const fileNames = readdirSync(postDir)
+        const postsData = fileNames.map((fileName) => {
+            const id = fileName.replace(/\.mdx/, "")
+
+            const fullPath = path.join(postDir, fileName)
+            const fileContents = readFileSync(fullPath, "utf-8")
+
+            const { data, content } = matter(fileContents)
+
+            return {
+                id,
+                data,
+                content,
+            }
+        })
+
+        const contents = await Promise.all(postsData)
+
+        return contents
+    }
+
+    return {
+        getPosts,
+    }
+}
+
+export default usePosts
