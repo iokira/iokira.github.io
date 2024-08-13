69747bfa8eca735624d110bc0f8b421932e05885
iokira <iam.iokiller@gmail.com>
2024-08-14 00:23:21 +0900

    update: getPostに分離

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -2,33 +2,34 @@ import { readFileSync, readdirSync } from "fs"
 import matter from "gray-matter"
 import path from "path"
 
-const postDir = path.join(process.cwd(), "src/pages/blog/posts")
+const postDir = path.join(process.cwd(), "src/posts")
 
 const usePosts = () => {
+    const getPost = async (id: string) => {
+        const fileName = id + ".mdx"
+        const fullPath = path.join(postDir, fileName)
+        const fileContents = readFileSync(fullPath, "utf-8")
+
+        const { data, content } = matter(fileContents)
+
+        const postData: PostData = {
+            title: data.title as string,
+            description: data.description as string,
+            tags: data.tags as string[],
+            createDate: data.createDate as Date,
+            updateDate: data.updateDate as Date,
+        }
+
+        return {
+            id,
+            postData,
+            content,
+        }
+    }
+
     const getPosts = async () => {
         const fileNames = readdirSync(postDir)
-        const postsData = fileNames.map((fileName) => {
-            const id = fileName.replace(/\.mdx/, "")
-
-            const fullPath = path.join(postDir, fileName)
-            const fileContents = readFileSync(fullPath, "utf-8")
-
-            const { data, content } = matter(fileContents)
-
-            const postData: PostData = {
-                title: data.title as string,
-                description: data.description as string,
-                tags: data.tags as string[],
-                createDate: data.createDate as Date,
-                updateDate: data.updateDate as Date,
-            }
-
-            return {
-                id,
-                postData,
-                content,
-            }
-        })
+        const postsData = fileNames.map((fileName) => getPost(fileName))
 
         const contents = await Promise.all(postsData)
 
@@ -36,6 +37,7 @@ const usePosts = () => {
     }
 
     return {
+        getPost,
         getPosts,
     }
 }
