3703820f93064092db476cd723a1348b0b85e9a5
iokira <iam.iokiller@gmail.com>
2024-08-14 03:28:08 +0900

    add: remarkToc rehypeSlug

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,8 @@ import 'prismjs/components/prism-bash.js'
 import 'prismjs/components/prism-rust.js'
 import 'prismjs/components/prism-typescript.js'
 import 'prismjs/components/prism-lua.js'
+import rehypeSlug from "rehype-slug"
+import remarkToc from "remark-toc"
 
 const nextConfig = {
     pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
@@ -17,8 +19,13 @@ const nextConfig = {
 
 const withMDX = createMDX({
     options: {
-        remarkPlugins: [remarkGfm, remarkFrontmatter, remarkMath],
-        rehypePlugins: [rehypePrism, rehypeMathjax],
+        remarkPlugins: [
+            remarkGfm, 
+            remarkFrontmatter, 
+            remarkMath, 
+            [remarkToc, { maxDepth: 3, heading: "目次" }]
+        ],
+        rehypePlugins: [rehypePrism, rehypeMathjax, rehypeSlug],
     }
 })
 
