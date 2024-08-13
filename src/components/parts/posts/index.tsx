4a12b922e943022529fe55f79a0e3a753bfc6581
iokira <iam.iokiller@gmail.com>
2024-08-13 22:48:56 +0900

    update: 記事の順番を変えられるように

--- src/components/parts/posts/index.tsx
+++ src/components/parts/posts/index.tsx
@@ -1,10 +1,40 @@
 import Post from "@/components/parts/post"
 import styles from "./style.module.scss"
+import { useEffect, useState } from "react"
+
+enum Order {
+    Asc,
+    Desc,
+}
 
 const Posts = ({ posts }: { posts: Post[] }) => {
+    const [order, setOrder] = useState<Order>(Order.Desc)
+    const [sortedPosts, setSortedPosts] = useState<Post[]>(posts)
+
+    useEffect(() => {
+        setSortedPosts(
+            posts.slice().sort((a, b) => {
+                if (order === Order.Asc) {
+                    return a.data.createDate < b.data.createDate ? -1 : 1
+                } else {
+                    return a.data.createDate > b.data.createDate ? -1 : 1
+                }
+            }),
+        )
+    }, [order])
+
     return (
         <div className={styles.posts}>
-            {posts.map((post) => (
+            <select
+                className={styles.select}
+                name="order"
+                value={order}
+                onChange={(e) => setOrder(Number(e.target.value))}
+            >
+                <option value={Order.Asc}>Asc</option>
+                <option value={Order.Desc}>Desc</option>
+            </select>
+            {sortedPosts.map((post) => (
                 <Post
                     key={post.id}
                     post={post}
