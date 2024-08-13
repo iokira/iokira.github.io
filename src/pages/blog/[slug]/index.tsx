0ddae0127f1e339ae30330622775ff3c5f9698b7
iokira <iam.iokiller@gmail.com>
2024-08-14 00:24:06 +0900

    add: 記事の型を作成

---
+++ src/pages/blog/[slug]/index.tsx
@@ -0,0 +1,47 @@
+import usePosts from "@/hooks/usePosts"
+import { MDXProvider } from "@mdx-js/react"
+import { GetStaticPaths, GetStaticProps } from "next"
+
+export const getStaticPaths = (async () => {
+    const { getPosts } = usePosts()
+    const posts: Post[] = await getPosts()
+    const paths = posts.map((post) => {
+        return {
+            params: {
+                slug: post.id,
+            },
+        }
+    })
+
+    return {
+        paths: paths,
+        fallback: false,
+    }
+}) satisfies GetStaticPaths
+
+export const getStaticProps = (async (context) => {
+    const { getPost } = usePosts()
+    const { params } = context
+    const post = await getPost(params.slug as string)
+
+    return {
+        props: {
+            post,
+        },
+    }
+}) satisfies GetStaticProps<{ post: Post }>
+
+const Slug = ({ post }: { post: Post }) => {
+    return (
+        <MDXProvider>
+            <h1>{post.postData.title}</h1>
+            <p>投稿日: {post.postData.createDate.toString()}</p>
+            {post.postData.updateDate != post.postData.createDate && (
+                <p>更新日: {post.postData.updateDate.toString()}</p>
+            )}
+            {post.content}
+        </MDXProvider>
+    )
+}
+
+export default Slug
