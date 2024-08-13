64c4e4dfc351b7718dda0263055b2ad2edd0f730
iokira <iam.iokiller@gmail.com>
2024-08-13 22:36:56 +0900

    update: updateDateがない場合createDateを表示するように

--- src/components/parts/post/index.tsx
+++ src/components/parts/post/index.tsx
@@ -2,9 +2,10 @@ import Link from "@/components/parts/link"
 import styles from "./style.module.scss"
 
 const Post = ({ post }: { post: Post }) => {
+    const date = post.data.updateDate ? post.data.updateDate + " (edited)" : post.data.createDate
     return (
         <article className={styles.post}>
-            <p className={styles.date}>{post.data.updateDate}</p>
+            <p className={styles.date}>{date}</p>
             <div className={styles.title}>
                 <Link href={"blog/posts/" + post.id}>{post.data.title}</Link>
             </div>
