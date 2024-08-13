b4da8be0a0616bee8e3c379a8b5b1c2c06637de4
iokira <iam.iokiller@gmail.com>
2024-08-13 20:00:14 +0900

    add: remark and rehype plugin

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,20 @@
 import createMDX from "@next/mdx"
+import rehypePrism from "rehype-prism"
+import remarkGfm from "remark-gfm"
+
+import 'prismjs/components/prism-lua.js'
 
 const nextConfig = {
     pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
     output: "export",
+    reactStrictMode: true,
 }
 
-const withMDX = createMDX()
+const withMDX = createMDX({
+    options: {
+        remarkPlugins: [remarkGfm],
+        rehypePlugins: [rehypePrism],
+    }
+})
 
 export default withMDX(nextConfig)
