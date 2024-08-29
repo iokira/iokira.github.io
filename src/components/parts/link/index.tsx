39031945135b765e3f6041806df3e7e05bd26ff3
iokira <iam.iokiller@gmail.com>
2024-08-30 00:21:05 +0900

    update: NextLinkのpropsも指定できるように変更

--- src/components/parts/link/index.tsx
+++ src/components/parts/link/index.tsx
@@ -1,10 +1,21 @@
 import NextLink from "next/link"
 import styles from "./style.module.scss"
+import { ReactNode } from "react"
 
-const Link = ({ href, children }) => {
+type LinkProps = {
+    href: string
+    children: ReactNode
+} & Pick<React.ComponentProps<typeof NextLink>, "target">
+
+const Link: React.FC<LinkProps> = ({ href, children, target }) => {
     return (
         <div className={styles.link}>
-            <NextLink href={href}>{children}</NextLink>
+            <NextLink
+                href={href}
+                target={target}
+            >
+                {children}
+            </NextLink>
         </div>
     )
 }
