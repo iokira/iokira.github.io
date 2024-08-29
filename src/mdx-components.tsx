685d9cdd80c29198a4ba36777b16d1c29e47bd81
iokira <iam.iokiller@gmail.com>
2024-08-30 00:20:45 +0900

    update: mdxでaタグを使用した場合target=_blankを指定するように変更

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,8 +1,17 @@
 import type { MDXComponents } from "mdx/types"
 import "prismjs/themes/prism-tomorrow.css"
+import Link from "./components/parts/link"
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
     return {
+        a: ({ href, children }) => (
+            <Link
+                href={href}
+                target="_blank"
+            >
+                {children}
+            </Link>
+        ),
         ...components,
     }
 }
