2b8ff30797ac59c45907bbb45b71fa6383b4751c
iokira <iam.iokiller@gmail.com>
2025-07-21 17:39:58 +0900

    update: site stack 作成

--- cdk/lib/site-stack.ts
+++ cdk/lib/site-stack.ts
@@ -4,5 +4,38 @@ import { Construct } from "constructs";
 export class SiteStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
+
+        const siteBucket = new cdk.aws_s3.Bucket(this, "SiteBucket", {
+            bucketName: "iokira-net",
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            autoDeleteObjects: true,
+        });
+
+        const distribution = new cdk.aws_cloudfront.Distribution(
+            this,
+            "SiteDistribution",
+            {
+                defaultRootObject: "index.html",
+                defaultBehavior: {
+                    origin: cdk.aws_cloudfront_origins.S3BucketOrigin.withOriginAccessControl(
+                        siteBucket,
+                    ),
+                    viewerProtocolPolicy:
+                        cdk.aws_cloudfront.ViewerProtocolPolicy
+                            .REDIRECT_TO_HTTPS,
+                },
+            },
+        );
+
+        new cdk.aws_s3_deployment.BucketDeployment(this, "DeploySite", {
+            sources: [],
+            destinationBucket: siteBucket,
+            distribution: distribution,
+            distributionPaths: ["/*"],
+        });
+
+        new cdk.CfnOutput(this, "Hosting URL", {
+            value: "https://" + distribution.distributionDomainName,
+        });
     }
 }
